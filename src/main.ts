d2f994719065294bcc7200de49ae1cedc887e9a7
Drew Kilb <drew.kilb@duke.edu>
2022-04-04 14:44:53 -0400

    chore: Clean up after myself when extension is deactivated
    This commit removes the two commands registered with Nova when the
    extension is deactivated. This should hopefully clean up memory
    leaks from listeners used by this extension.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { editStyles } from './edit-styles'
 import { pickSource } from './pick-source'
 
+const disposables = new CompositeDisposable()
+
 function activate() {
   // Do work when the extension is activated
-  // nova.commands.invoke("theme-mangler.pickSource");
+  disposables.add(
+    nova.commands.register('theme-mangler.pickSource', pickSource)
+  )
+  disposables.add(
+    nova.commands.register('theme-mangler.editStyles', editStyles)
+  )
 }
 
 function deactivate() {
   // Clean up state before the extension is deactivated
+  disposables.dispose()
 }
-
-nova.commands.register('theme-mangler.pickSource', pickSource)
-
-nova.commands.register('theme-mangler.editStyles', editStyles)
 
 export { activate, deactivate }
