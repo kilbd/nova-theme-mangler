a8d2332fd833dd81102728532abfc0bea45ea07b
Drew Kilb <drew.kilb@duke.edu>
2022-04-04 10:41:39 -0400

    feat: Generate custom theme when user styles are saved
    This commit adds listeners for saving and closing the user styles
    document. When closed, it destroys the listeners. When styles are
    saved, it generates the custom theme. I may need to install as a
    real extension to test, because right now Nova doesn't pick up the
    theme saved in 'Application Support'.

--- src/edit-styles.ts
+++ src/edit-styles.ts
@@ -4,7 +4,12 @@ async function editStyles(workspace: Workspace) {
   const editor = await workspace.openFile(
     `${extensionDir}/data/user-styles.css`
   )
-  console.log(editor?.document.path)
+  const saveDisposable = editor?.onDidSave(generateTheme)
+  let closeDisposable: Disposable | undefined
+  closeDisposable = editor?.onDidDestroy((_editor) => {
+    saveDisposable?.dispose()
+    closeDisposable?.dispose()
+  })
 }
 
 function checkStylesFile(extRoot: string) {
@@ -22,6 +27,34 @@ function createDirIfMissing(path: string) {
   if (!nova.fs.access(path, nova.fs.F_OK)) {
     nova.fs.mkdir(path)
   }
+}
+
+function generateTheme(editor: TextEditor) {
+  let themeDir = `${nova.extension.globalStoragePath}/Themes`
+  // Nova doesn't seem to pick up themes in global storage path for dev mode.
+  // Here's hoping it does when not in dev mode!
+  if (nova.inDevMode()) {
+    themeDir = `${nova.extension.path}/Themes`
+  }
+  createDirIfMissing(themeDir)
+  const mangledTheme = nova.fs.open(`${themeDir}/My Perfect Theme.css`, 'w')
+  const source = nova.config.get('com.kilb.theme-mangler.source') as
+    | string
+    | null
+  if (source) {
+    const themeFile = nova.fs.open(source, 'r') as FileTextMode
+    let line: string | null
+    do {
+      line = themeFile.readline()
+      if (line !== null) {
+        mangledTheme.write(line)
+      }
+    } while (line?.includes('\n'))
+    mangledTheme.write('\n')
+  }
+  let userStyles = editor.getTextInRange(new Range(0, editor.document.length))
+  mangledTheme.write(userStyles)
+  mangledTheme.close()
 }
 
 export { editStyles }
